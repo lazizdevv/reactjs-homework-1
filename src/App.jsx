58357cf05cc596b853261e4c2f18a4a89492a266
Lazizbek <oddiybolakay05@gmail.com>
2024-06-23 15:34:13 +0500

    branch to main

--- src/App.jsx
+++ src/App.jsx
@@ -26,25 +26,27 @@ function App() {
   return(
     <>
     <main>
-      <div className="container">
-        <h1 id="book-type">Янги қўшилганлар</h1>
-        <div id="new-adds">
-          <ProductCard img={img1} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
-          <ProductCard img={img2} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
-          <ProductCard img={img3} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
-          <ProductCard img={img4} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
-          <ProductCard img={img5} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
-        </div>
+      <section id="product">
+        <div className="container">
+          <h1 id="book-type">Янги қўшилганлар</h1>
+          <div id="new-adds">
+            <ProductCard img={img1} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
+            <ProductCard img={img2} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
+            <ProductCard img={img3} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
+            <ProductCard img={img4} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
+            <ProductCard img={img5} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book}/>
+          </div>
 
-        <h1 id="book-type">Аудио китоблар</h1>
-        <div id="audio-books">
-          <ProductCard img={img6} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
-          <ProductCard img={img7} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
-          <ProductCard img={img5} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
-          <ProductCard img={img8} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
-          <ProductCard img={img9} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
+          <h1 id="book-type">Аудио китоблар</h1>
+          <div id="audio-books">
+            <ProductCard img={img6} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
+            <ProductCard img={img7} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
+            <ProductCard img={img5} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
+            <ProductCard img={img8} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
+            <ProductCard img={img9} name={"code-8"} info={"SIYOSAT, FANTASTIKA"} star={star} rating={"4.7"} phone={headPhone} book={book} hidden={true}/>
+          </div>
         </div>
-      </div>
+      </section>
     </main>
 
     <Footer/>
